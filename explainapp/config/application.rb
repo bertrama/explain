f58df194f49418c0ffac01ffa534b9d89f652e4b
Albert Bertram <bertrama@umich.edu>
2018-05-06 08:18:54 -0400

    Rails 5.2.0 compatibility changes.

--- explainapp/config/application.rb
+++ explainapp/config/application.rb
@@ -27,15 +27,8 @@ module Explainapp
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-config.sass.debug_info = true
-config.sass.line_comments = false
-
-    # I couldn't find a better way to do this in rails 3.0.20.
-    #config.middleware.insert_after(
-      #"::ActionDispatch::Static",
-      #ActionDispatch::Static,
-      #Gem.loaded_specs['lazy_high_charts'].full_gem_path + '/vendor/assets'
-    #)
+    config.sass.debug_info = true
+    config.sass.line_comments = false
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/../explain/lib)
